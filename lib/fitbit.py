71aec9fa22f8e30dcf4d28f5b72f4eaece6c2476
kojimekata-star <kojimekata@gmail.com>
2026-07-05 07:57:11 +0900

    Update fitbit.py

--- lib/fitbit.py
+++ lib/fitbit.py
@@ -8,6 +8,7 @@ from lib.database import get_conn
 SCOPES = [
     "https://www.googleapis.com/auth/googlehealth.sleep.readonly",
     "https://www.googleapis.com/auth/googlehealth.activity_and_fitness.readonly",
+    "https://www.googleapis.com/auth/health.metrics_and_measurements.readonly",
 ]
 
 def _creds():
